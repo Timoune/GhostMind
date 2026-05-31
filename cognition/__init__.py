f6fba4a1a10e186a804245df4e18ab3e0b2fb8e8
Timoune <sameth700@proton.me>
2026-05-31 02:33:22 +0000

    Update __init__.py

--- cognition/__init__.py
+++ cognition/__init__.py
@@ -1,25 +1,28 @@
 from .intent_engine import IntentEngine
-from .decomposition_engine import (
-    DecompositionEngine,
-)
+from .decomposition_engine import DecompositionEngine
 from .planning_engine import PlanningEngine
-from .reflection_engine import (
-    ReflectionEngine,
-)
+from .reflection_engine import ReflectionEngine
 from .decision_engine import DecisionEngine
 from .step_executor import StepExecutor
-from .decision_ledger import (
-    DecisionLedger,
-)
+from .decision_ledger import DecisionLedger
 
 from .cognition_types import (
     IntentAnalysis,
     TaskNode,
     DecompositionResult,
     ExecutionPlan,
+    AgentResult,
+    MultiAgentResult,
 )
 
 from .cognition_record import (
     ReflectionResult,
     CognitionRecord,
 )
+
+from .human_approval_gate import (
+    HumanApprovalGate,
+    ApprovalRequest,
+)
+
+from .multi_agent_orchestrator import MultiAgentOrchestrator
